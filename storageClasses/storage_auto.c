fd08702195ff7ebd41208f3942e0ccf215c15e78
thephotogenicbug <navinkumarachari@gmail.com>
2025-12-27 11:17:39 +0530

    storage classes

---
+++ storageClasses/storage_auto.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main()
+{
+    auto int x = 10;
+    printf("%d", x);
+    return 0;
+}
---
+++ storageClasses/storage_static.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+void counter()
+{
+
+    static int count = 0;
+    count++;
+    printf("Count = %d\n", count);
+}
+
+int main()
+{
+    counter();
+    counter();
+
+    return 0;
+}
